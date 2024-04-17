a724994550d3cc540e5cfc1b8627f4e0509a5c54
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-04-17 05:05:22 +0200

    m

--- download_from_youtube.py
+++ download_from_youtube.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
-#variables excel
+#Variables excel
 file_path = 'enlaces_videos/enlaces.xlsx'
 sheet_name = 'Hoja1'
 column_name = 'Videos'
@@ -27,3 +27,4 @@ def login():
         
     gauth.SaveCredentialsFile(directorio_credenciales)
     credenciales = GoogleDrive(gauth)
+    return credenciales
